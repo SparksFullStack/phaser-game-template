aea600c930a87774a29df1a406b4dfd40f564b46
SparksFullStack <csparks@antioch.edu>
2019-01-19 17:27:07 -0800

    character of 'mainChar' animated

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -10,6 +10,22 @@ class SceneMain extends Phaser.Scene {
     create() {
         // defines all objects for the scene
         this.mainChar = this.add.sprite(game.config.width / 2, game.config.height / 2, "mainChar");
+
+        this.anims.create({
+            key: 'walk',
+            frames: [
+                { key: 'mainChar',frame:0 },
+                { key: 'mainChar',frame:1 },
+                { key: 'mainChar',frame:2 },
+                { key: 'mainChar',frame:3 },
+                { key: 'mainChar',frame:4 },
+                { key: 'mainChar',frame:5 },
+            ],
+            frameRate: 8,
+            repeat: -1
+        });
+
+        this.mainChar.play('walk');
     }
     update() {
         // constantly runnning loop
