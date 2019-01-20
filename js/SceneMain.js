0ec8404b12af0a5079218c56a4a83d09b855c7c0
SparksFullStack <csparks@antioch.edu>
2019-01-19 17:19:54 -0800

    added sprite sheet

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -5,9 +5,11 @@ class SceneMain extends Phaser.Scene {
     preload()
     {
         // preload all assets
+        this.load.spritesheet('mainChar', 'images/mainChar.png', { frameWidth: 120, frameHeight: 200 });
     }
     create() {
         // defines all objects for the scene
+        this.mainChar = this.add.sprite(game.config.width / 2, game.config.height / 2, "mainChar");
     }
     update() {
         // constantly runnning loop
