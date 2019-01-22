fa13dd3e9e189b917533fd4a0fb0fcecd79da129
SparksFullStack <csparks@antioch.edu>
2019-01-21 20:28:38 -0800

    animation running with a Tween; fixed scoping issue with the onCompleteHandler for the Tween; added text to the Scene

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -9,19 +9,38 @@ class SceneMain extends Phaser.Scene {
     }
     create() {
         // defines all objects for the scene
-        this.mainChar = this.add.sprite(game.config.width / 2, game.config.height / 2, "mainChar");
+        // this.mainChar = this.add.sprite(game.config.width / 2, game.config.height / 2, "mainChar");
 
-        var frameNames = this.anims.generateFrameNumbers('mainChar'); // auto generates the frames from the sprite sheet
+        // var frameNames = this.anims.generateFrameNumbers('mainChar'); // auto generates the frames from the sprite sheet
 
-        this.anims.create({
-            key: 'walk',
-            frames: frameNames,
-            frameRate: 8,
-            repeat: -1
-        });
+        // this.anims.create({
+        //     key: 'walk',
+        //     frames: frameNames,
+        //     frameRate: 8,
+        //     repeat: -1
+        // });
 
-        this.mainChar.play('walk');
+        // this.mainChar.play('walk');
+        // this.doWalk();   
+
+        this.testText = this.add.text(game.config.width / 2, game.config.height / 2, "Howdy");
+        this.testText.setOrigin(.5, .5);
     }
+
+    doWalk(){
+        this.tweens.add({ 
+            targets: this.mainChar, 
+            duration: 1000, 
+            x: game.config.width, 
+            onComplete: this.onCompleteHandler ,
+            onCompleteParams: this
+        });
+    }
+
+    onCompleteHandler(tween, targets, scope){
+        scope.mainChar.x = 0;
+    }
+
     update() {
         // constantly runnning loop
     }
